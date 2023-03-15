65bcd870970405ce66f67acd4816e01917f954a6
Michelle2401 <116472057+Michelle2401@users.noreply.github.com>
2023-03-15 00:42:55 -0600

    productos

--- Facturacion/Vista/ProductosForm.Designer.cs
+++ Facturacion/Vista/ProductosForm.Designer.cs
@@ -29,10 +29,248 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CodigotextBox = new System.Windows.Forms.TextBox();
+            this.DescripciontextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PreciotextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExistenciatextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ImagenpictureBox = new System.Windows.Forms.PictureBox();
+            this.AdjuntarImagenbutton = new System.Windows.Forms.Button();
+            this.Cancelarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Modificarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.ProductosdataGridView = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosdataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo:";
+            // 
+            // CodigotextBox
+            // 
+            this.CodigotextBox.BackColor = System.Drawing.Color.White;
+            this.CodigotextBox.Enabled = false;
+            this.CodigotextBox.Location = new System.Drawing.Point(108, 9);
+            this.CodigotextBox.Name = "CodigotextBox";
+            this.CodigotextBox.Size = new System.Drawing.Size(196, 25);
+            this.CodigotextBox.TabIndex = 1;
+            // 
+            // DescripciontextBox
+            // 
+            this.DescripciontextBox.BackColor = System.Drawing.Color.White;
+            this.DescripciontextBox.Enabled = false;
+            this.DescripciontextBox.Location = new System.Drawing.Point(108, 50);
+            this.DescripciontextBox.Name = "DescripciontextBox";
+            this.DescripciontextBox.Size = new System.Drawing.Size(196, 25);
+            this.DescripciontextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descripcion:";
+            // 
+            // PreciotextBox
+            // 
+            this.PreciotextBox.BackColor = System.Drawing.Color.White;
+            this.PreciotextBox.Enabled = false;
+            this.PreciotextBox.Location = new System.Drawing.Point(108, 143);
+            this.PreciotextBox.Name = "PreciotextBox";
+            this.PreciotextBox.Size = new System.Drawing.Size(196, 25);
+            this.PreciotextBox.TabIndex = 7;
+            this.PreciotextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PreciotextBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Precio:";
+            // 
+            // ExistenciatextBox
+            // 
+            this.ExistenciatextBox.BackColor = System.Drawing.Color.White;
+            this.ExistenciatextBox.Enabled = false;
+            this.ExistenciatextBox.Location = new System.Drawing.Point(108, 102);
+            this.ExistenciatextBox.Name = "ExistenciatextBox";
+            this.ExistenciatextBox.Size = new System.Drawing.Size(196, 25);
+            this.ExistenciatextBox.TabIndex = 5;
+            this.ExistenciatextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ExistenciatextBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Existencia:";
+            // 
+            // ImagenpictureBox
+            // 
+            this.ImagenpictureBox.BackColor = System.Drawing.Color.White;
+            this.ImagenpictureBox.Location = new System.Drawing.Point(357, 9);
+            this.ImagenpictureBox.Name = "ImagenpictureBox";
+            this.ImagenpictureBox.Size = new System.Drawing.Size(118, 137);
+            this.ImagenpictureBox.TabIndex = 8;
+            this.ImagenpictureBox.TabStop = false;
+            // 
+            // AdjuntarImagenbutton
+            // 
+            this.AdjuntarImagenbutton.BackColor = System.Drawing.Color.White;
+            this.AdjuntarImagenbutton.Enabled = false;
+            this.AdjuntarImagenbutton.Image = global::Vista.Properties.Resources.lupa;
+            this.AdjuntarImagenbutton.Location = new System.Drawing.Point(481, 104);
+            this.AdjuntarImagenbutton.Name = "AdjuntarImagenbutton";
+            this.AdjuntarImagenbutton.Size = new System.Drawing.Size(44, 42);
+            this.AdjuntarImagenbutton.TabIndex = 9;
+            this.AdjuntarImagenbutton.UseVisualStyleBackColor = false;
+            // 
+            // Cancelarbutton
+            // 
+            this.Cancelarbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Cancelarbutton.Enabled = false;
+            this.Cancelarbutton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelarbutton.Location = new System.Drawing.Point(431, 189);
+            this.Cancelarbutton.Name = "Cancelarbutton";
+            this.Cancelarbutton.Size = new System.Drawing.Size(86, 35);
+            this.Cancelarbutton.TabIndex = 23;
+            this.Cancelarbutton.Text = "Cancelar";
+            this.Cancelarbutton.UseVisualStyleBackColor = false;
+            this.Cancelarbutton.Click += new System.EventHandler(this.Cancelarbutton_Click);
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Eliminarbutton.Enabled = false;
+            this.Eliminarbutton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eliminarbutton.Location = new System.Drawing.Point(344, 190);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(81, 34);
+            this.Eliminarbutton.TabIndex = 22;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.UseVisualStyleBackColor = false;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Guardarbutton.Enabled = false;
+            this.Guardarbutton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Guardarbutton.Location = new System.Drawing.Point(262, 190);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(76, 33);
+            this.Guardarbutton.TabIndex = 21;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = false;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // Modificarbutton
+            // 
+            this.Modificarbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Modificarbutton.Enabled = false;
+            this.Modificarbutton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Modificarbutton.Location = new System.Drawing.Point(167, 190);
+            this.Modificarbutton.Name = "Modificarbutton";
+            this.Modificarbutton.Size = new System.Drawing.Size(89, 34);
+            this.Modificarbutton.TabIndex = 20;
+            this.Modificarbutton.Text = "Modificar";
+            this.Modificarbutton.UseVisualStyleBackColor = false;
+            this.Modificarbutton.Click += new System.EventHandler(this.Modificarbutton_Click);
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Nuevobutton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nuevobutton.Location = new System.Drawing.Point(85, 190);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(76, 34);
+            this.Nuevobutton.TabIndex = 19;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.UseVisualStyleBackColor = false;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
+            // 
+            // ProductosdataGridView
+            // 
+            this.ProductosdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductosdataGridView.Location = new System.Drawing.Point(0, 230);
+            this.ProductosdataGridView.Name = "ProductosdataGridView";
+            this.ProductosdataGridView.Size = new System.Drawing.Size(583, 206);
+            this.ProductosdataGridView.TabIndex = 24;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ProductosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ProductosForm";
+            this.ClientSize = new System.Drawing.Size(586, 440);
+            this.Controls.Add(this.ProductosdataGridView);
+            this.Controls.Add(this.Cancelarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Modificarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.AdjuntarImagenbutton);
+            this.Controls.Add(this.ImagenpictureBox);
+            this.Controls.Add(this.PreciotextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ExistenciatextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DescripciontextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CodigotextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ProductosForm";
+            this.Text = "Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosdataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CodigotextBox;
+        private System.Windows.Forms.TextBox DescripciontextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PreciotextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ExistenciatextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox ImagenpictureBox;
+        private System.Windows.Forms.Button AdjuntarImagenbutton;
+        private System.Windows.Forms.Button Cancelarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Modificarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.DataGridView ProductosdataGridView;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- Facturacion/Vista/ProductosForm.cs
+++ Facturacion/Vista/ProductosForm.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+﻿using Entidades;
+using System;
 using System.Windows.Forms;
 
 namespace Vista
@@ -13,6 +9,142 @@ namespace Vista
         public ProductosForm()
         {
             InitializeComponent();
+        }
+        string operacion;
+        Producto producto;
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            operacion = "Nuevo";
+            HabilitarControles();
+        }
+        private void HabilitarControles()
+        {
+            CodigotextBox.Enabled = true;
+            DescripciontextBox.Enabled = true;
+            ExistenciatextBox.Enabled = true;
+            PreciotextBox.Enabled = true;
+            AdjuntarImagenbutton.Enabled = true;
+            Guardarbutton.Enabled = true;
+            Cancelarbutton.Enabled = true;
+            Nuevobutton.Enabled = false;
+        }
+        //private void LimpiarControles()
+        //{
+        // CodigotextBox.Clear();
+        //DescripciontextBox.Clear();
+        //ExistenciatextBox.Clear();
+        //PreciotextBox.Clear();
+        //EstaActivoCheckBox.Checked = false;
+        // ImagenPictureBox.Image = null;
+        //producto = null;
+        // }
+        private void DeshabilitarControles()
+        {
+            CodigotextBox.Enabled = false;
+            DescripciontextBox.Enabled = false;
+            ExistenciatextBox.Enabled = false;
+            PreciotextBox.Enabled = false;
+            AdjuntarImagenbutton.Enabled = false;
+            Guardarbutton.Enabled = false;
+            Cancelarbutton.Enabled = false;
+            Nuevobutton.Enabled = true;
+        }
+
+        private void Cancelarbutton_Click(object sender, EventArgs e)
+        {
+            DeshabilitarControles();
+
+        }
+
+        private void Modificarbutton_Click(object sender, EventArgs e)
+        {
+            operacion = "Modificar";
+
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            producto = new Producto();
+            producto.Codigo = CodigotextBox.Text;
+            producto.Descripcion = DescripciontextBox.Text;
+            producto.Precio = Convert.ToDecimal(PreciotextBox.Text);
+            producto.Existencia = Convert.ToInt32(ExistenciatextBox.Text);
+
+
+            if (ImagenpictureBox.Image != null)
+            {
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                ImagenpictureBox.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                producto.Foto = ms.GetBuffer();
+            }
+
+            if (operacion == "Nuevo")
+            {
+                if (string.IsNullOrEmpty(CodigotextBox.Text))
+                {
+                    errorProvider1.SetError(CodigotextBox, "Ingrese un código");
+                    CodigotextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(DescripciontextBox.Text))
+                {
+                    errorProvider1.SetError(DescripciontextBox, "Ingrese una descripción");
+                    DescripciontextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(ExistenciatextBox.Text))
+                {
+                    errorProvider1.SetError(ExistenciatextBox, "Ingrese una existencia");
+                    ExistenciatextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(PreciotextBox.Text))
+                {
+                    errorProvider1.SetError(PreciotextBox, "Ingrese un precio");
+                    PreciotextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+
+
+
+
+            }
+        }
+
+        private void ExistenciatextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void PreciotextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && (e.KeyChar != '.') && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+
         }
     }
 }
